96920a1fc1b7e0f97798b290c0d0eaf6bc838557
qinmudi_zyb <qinmudi_zyb@baidu.com>
2016-07-02 21:30:56 +0800

    fis3-wiiyii
    初始化导入

---
+++ README.md
@@ -0,0 +1,18 @@
+## fis3-wiiyii
+
+基于 FIS3 的针对yii框架和smarty模板的前端工程解决方案
+
+### 文档
+
+- 构建工具文档参见 https://github.com/fex-team/fis3
+
+### 使用方法
+
+**安装**
+
+```
+npm install -g fis3
+npm install -g fis3-wiiyii
+```
+
+未完结，待续…
---
+++ index.js
@@ -0,0 +1,163 @@
+/**
+ * fis3和yii整合解决方案
+ */
+
+var path = require('path');
+
+module.exports = function(fis, isMount) {
+    var sets = {
+        'namespace': '',
+        'static': 'static',
+        'template': 'template',
+        'views': 'views',
+        'smarty': {
+            'left_delimiter': '{%',
+            'right_delimiter': '%}'
+        }
+    };
+
+    fis.set('server.type', 'smarty');
+
+    var matchRules = {
+        // all release to $static dir
+        '*': {
+            release: '/${static}/${namespace}/$0'
+        },
+        '*.{js,css,scss}': {
+            useHash: true
+        },
+        '*.js': {
+            optimizer: fis.plugin('uglify-js')
+        },
+        '*.scss': {
+            parser: fis.plugin('sass'),
+            rExt: '.css'
+        },
+        '*.{css,scss}': {
+            optimizer: fis.plugin('clean-css'),
+            useSprite: true
+        },
+        '::image': {
+            useHash: true
+        },
+        '*.png': {
+            optimizer: fis.plugin('png-compressor')
+        },
+        '/static/module/**.{js,es}': {
+            isMod: true
+        },
+        '/(**.tpl)': {
+            preprocessor: fis.plugin('extlang'),
+            optimizer: [
+                fis.plugin('smarty-xss'),
+                fis.plugin('html-compress')
+            ],
+            useMap: true,
+            release: '/${template}/${namespace}/$1'
+        },
+        '*.{tpl,js}': {
+            useSameNameRequire: true
+        },
+        // page dir
+        '/page/**.tpl': {
+            // 标记是否是个页面，向下兼容
+            extras: {
+                isPage: true
+            }
+        },
+        // widget
+        '/(widget/**.tpl)': {
+            url: '${namespace}/$1',
+            useMap: true,
+        },
+        '/widget/{*.{js,css},**/*.{js,css}}': {
+            isMod: true
+        },
+        '/{plugin/**.*,smarty.conf,domain.conf,**.php}': {
+            release: '$0'
+        },
+        'server.conf': {
+            release: '/server-conf/${namespace}.conf'
+        },
+        '/static/(**)': {
+            release: '/${static}/${namespace}/$1'
+        },
+        // test & config
+        '/(test)/(**)': {
+            useMap: false,
+            release: '/$1/${namespace}/$2'
+        },
+        '/(config)/(**)': {
+            useMap: false,
+            release: '/$1/${namespace}/$2'
+        },
+        '${namespace}-map.json': {
+            release: '/config/$0'
+        },
+        '*.sh': {
+            release: '$0'
+        },
+        '::package': {
+            prepackager: [
+                fis.plugin('widget-inline'),
+                fis.plugin('js-i18n')
+            ]
+        }
+    };
+
+    function mount() {
+        // smarty
+        fis.set('system.localNPMFolder', path.join(__dirname, 'node_modules'));
+
+        // since fis3@3.3.21
+        // 帮当前目录的查找提前在 global 查找的前面，同时又保证 local 的查找是优先的。
+        if (fis.require.paths && fis.require.paths.length) {
+            fis.require.paths.splice(1, 0, path.join(__dirname, 'node_modules'));
+        }
+
+        fis.util.map(sets, function(key, value) {
+            fis.set(key, value);
+        });
+
+        fis.util.map(matchRules, function(selector, rules) {
+            fis.match(selector, rules);
+        });
+
+        // 模块化支持
+        fis.hook('commonjs');
+
+        // map.json
+        fis.match('::package', {
+            postpackager: function createMap(ret) {
+                var path = require('path')
+                var root = fis.project.getProjectPath();
+                var map = fis.file.wrap(path.join(root, fis.get('namespace') ? fis.get('namespace') + '-map.json' : 'map.json'));;
+                map.setContent(JSON.stringify(ret.map, null, map.optimizer ? null : 4));
+                ret.pkg[map.subpath] = map;
+            }
+        });
+    }
+
+    if (isMount !== false) {
+        mount();
+    }
+
+    //远程部署设置
+    fis.media('remote').match('/components/**', {
+        release: '/fe/${static}/$&'
+    }).match('/static/(**)', {
+        release: '/fe/${static}/${namespace}/$1'
+    }).match('/widget/**.{es,js,css,scss,png,ttf,woff,svg,woff2}', {
+        release: '/fe/${static}/${namespace}/$1'
+    }).match(/\/page\/(.+?)\/(.+?)\/(.+)/i, {
+        release: '/modules/$1/views/$2/$3'
+    }).match('/{layout,widget}/**.tpl', {
+        release: '/views/${namespace}/$0'
+    });
+
+    return {
+        loadPath: path.join(__dirname, 'node_modules'),
+        sets: sets,
+        matchRules: matchRules
+    }
+};
